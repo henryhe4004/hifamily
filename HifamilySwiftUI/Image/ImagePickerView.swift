dc9b28833b81c13d06626e9966cb73b8e6686949
心中有党成绩理想 <2544967914@qq.com>
2021-09-19 20:12:51 +0800

    8:12

--- HifamilySwiftUI/Image/ImagePickerView.swift
+++ HifamilySwiftUI/Image/ImagePickerView.swift
@@ -22,10 +22,50 @@ struct ImagePickerView: View {
 }
 struct MediaPicker: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> YPImagePicker {
-        let config = YPImagePickerConfiguration()
+        var config = YPImagePickerConfiguration()
+        //是否可以滑动
+        config.isScrollToChangeModesEnabled = false
+        //是否只能拍摄正方形
+        config.onlySquareImagesFromCamera = false
+        //默认打开前置摄像头
+        config.usesFrontCamera = true
+        //相册名
+        config.albumName = "Hi family"
+        //默认打开为相册
+        config.startOnScreen = YPPickerScreen.library
+        //展示啥
+        config.screens = [.library,.photo,.video]
+        //提供裁剪
+        config.showsCrop = .rectangle(ratio: 30)
+        //图片大小
+        config.targetImageSize = YPImageSize.original
+        //加功能
+        config.overlayView = UIView()
+        //是否隐藏状态栏
+        config.hidesStatusBar = false
+        config.hidesBottomBar = false
+        config.bottomMenuItemSelectedTextColour = UIColor(Color("AccentColor"))
+        config.bottomMenuItemUnSelectedTextColour = UIColor(Color.white)
+        //最大5倍变焦
+        config.maxCameraZoomFactor = 5.0
+        config.library.defaultMultipleSelection = true
+        //图片和视频
+        config.library.mediaType = YPlibraryMediaType.photo
+        config.library.maxNumberOfItems = 9
+        //不要预处理为最近一张图片
+        config.library.preSelectItemOnMultipleSelection = false;
+        config.library.spacingBetweenItems = 3
+        config.library.skipSelectionsGallery = false
+        
+        config.gallery.hidesRemoveButton = false
+        
         
         let picker = YPImagePicker(configuration: config)
-        picker.didFinishPicking { [unowned picker] items, _ in
+
+        picker.didFinishPicking { [unowned picker] items,  cancelled in
+            if cancelled{
+                print("用户按了取消按钮")
+            }
             if let photo = items.singlePhoto {
                 print(photo.fromCamera) // Image source (camera or library)
                 print(photo.image) // Final image selected by the user
---
+++ HifamilySwiftUI/Image/IssueUIView.swift
@@ -0,0 +1,22 @@
+//
+//  IssueUIView.swift
+//  HifamilySwiftUI
+//
+//  Created by 潘炳名 on 2021/9/19.
+//
+
+import SwiftUI
+
+struct IssueUIView: View {
+    var body: some View {
+        VStack{
+            Text("时光记录人")
+        }
+    }
+}
+
+struct IssueUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        IssueUIView()
+    }
+}
