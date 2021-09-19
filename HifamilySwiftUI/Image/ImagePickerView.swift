c704d5f976d7d5f08cce59a60362f48a11d65ac8
心中有党成绩理想 <2544967914@qq.com>
2021-09-19 08:51:01 +0800

    第三方包

--- HifamilySwiftUI/Image/ImagePickerView.swift
+++ HifamilySwiftUI/Image/ImagePickerView.swift
@@ -6,13 +6,60 @@
 //
 
 import SwiftUI
+import YPImagePicker
 
 struct ImagePickerView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    @State private var showYPImagePickerView = true
+        
+       var body: some View {
+           VStack {
+            HStack {
+                Image("Iconly-Bulk-Setting")
+                    .resizable()
+                    .frame(width:23,
+                           height:23,
+                           alignment:.center)
+                Spacer()
+                Text("Family album")
+                    .foregroundColor(Color("AccentColor"))
+                    .font(.system(size: 18))
+                Spacer()
+                Image("Iconly-Bulk-Plus")
+                    .resizable()
+                    .frame(width:23,
+                           height:23,
+                           alignment:.center)
+            }.padding()
+                   MediaPicker()
+               
+           }
 
+       }
+}
+struct MediaPicker: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> YPImagePicker {
+        let config = YPImagePickerConfiguration()
+        
+        let picker = YPImagePicker(configuration: config)
+        picker.didFinishPicking { [unowned picker] items, _ in
+            if let photo = items.singlePhoto {
+                print(photo.fromCamera) // Image source (camera or library)
+                print(photo.image) // Final image selected by the user
+                print(photo.originalImage) // original image selected by the user, unfiltered
+                print(photo.modifiedImage ?? "not modified !") // Transformed image, can be nil
+                print(photo.exifMeta ?? "no exif metadata") // Print exif meta data of original image."
+            }
+            picker.dismiss(animated: true, completion: nil)
+        }
+    
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: YPImagePicker, context: Context) {}
+    
+    typealias UIViewControllerType = YPImagePicker
+    
+}
 struct ImagePickerView_Previews: PreviewProvider {
     static var previews: some View {
         ImagePickerView()
